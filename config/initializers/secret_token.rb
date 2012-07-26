167ea385992033f3785bd997ffd8e28d26c02183
Faraaz Khan <faraaz@rationalizeit.us>
2012-07-26 19:35:05 -0400

    scaffolding

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PRL::Application.config.secret_token = '18f28557689a3198568f6d5b68f368de52bf4693791622e2d82a32251e1a437f07a8051ecc2d67e87ed2c994083b040c8242eecad54f72154a6e20f6688336c8'
